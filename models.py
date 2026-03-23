7793eda06dd819c450f53e720d7a3d2204e82930
MatasZos <B00163194@mytudublin.ie>
2026-03-23 15:44:03 +0000

    customer and car model

--- models.py
+++ models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
 class Customer(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state_province = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.name
+        return f"{self.first_name} {self.last_name}"
+
+
+class Car(models.Model):
+    serial_number = models.CharField(max_length=100)
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    colour = models.CharField(max_length=50)
+    year = models.IntegerField()
+    car_for_sale = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.make} {self.model} ({self.year})"
