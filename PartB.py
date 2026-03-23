02bcd1cab7d7cf24ecb527c805b7a8f0ea32cd6f
MatasZos <B00163194@mytudublin.ie>
2026-03-23 15:29:18 +0000

    object instance class test

--- PartB.py
+++ PartB.py
@@ -7,3 +7,16 @@ class TestMusicClasses(unittest.TestCase):
         self.album = Album("Fearless", "Taylor Swift", 2008)
         self.song = Song("Love Story", "Taylor Swift", 2008)
         self.playlist = Playlist("My Playlist")
+
+    """unit test for if an object is an instance of a class"""
+    def test_artist_instance(self):
+        self.assertIsInstance(self.artist, Artist)
+
+    def test_album_instance(self):
+        self.assertIsInstance(self.album, Album)
+
+    def test_song_instance(self):
+        self.assertIsInstance(self.song, Song)
+
+    def test_playlist_instance(self):
+        self.assertIsInstance(self.playlist, Playlist)
