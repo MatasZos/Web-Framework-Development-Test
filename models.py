df236ba13e413d0a29e761093e157df51fa02916
MatasZos <B00163194@mytudublin.ie>
2026-03-23 15:46:30 +0000

    salesperson model

--- models.py
+++ models.py
@@ -24,3 +24,10 @@ class Car(models.Model):
 
     def __str__(self):
         return f"{self.make} {self.model} ({self.year})"
+
+class Salesperson(model.Model):
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
