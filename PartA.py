b018906010d1afb2cbb0b852b02f1eccc36b74eb
MatasZos <B00163194@mytudublin.ie>
2026-03-23 14:42:12 +0000

    defining and printing attributes

--- PartA.py
+++ PartA.py
@@ -9,4 +9,22 @@ class Artist:
         self.country = country
         self.albums = []    
         self.songs = []
+        
+    def display_info(self):
+        print("Artist name is:", self.name)
+        print("Date of birth is:", self.dob)
+        print("Country:", self.country)
+        print("Albums made:")
+        for album in self.albums:
+            print(" , ", album.title)
+        print("Songs made:")
+        for song in self.songs:
+            print(" , ", song.title)
+        print("-"*40)
+    
+    def add_album(self,album):
+        self.albums.append(self.album)
+    
+    def add_song(self,song):
+        self.songs.append(song)
 
