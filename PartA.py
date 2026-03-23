120e50b7919adb73a138e6f968729321e0d65412
MatasZos <B00163194@mytudublin.ie>
2026-03-23 14:35:06 +0000

    class artist

---
+++ PartA.py
@@ -0,0 +1,12 @@
+
+import random
+
+
+class Artist:
+    def __init__(self, name, dob, country):
+        self.name = name
+        self.dob = dob
+        self.country = country
+        self.albums = []    
+        self.songs = []
+
